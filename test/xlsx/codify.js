ec8c22c00e32c8ac6cede05660af6ce59b4c5df1
drewlab <drew@dictadata.org>
2024-04-16 16:56:05 -0500

    add voter_registration codify test

--- test/xlsx/codify.js
+++ test/xlsx/codify.js
@@ -14,7 +14,8 @@ async function tests() {
   logger.info("=== codify foofile.xlsx|foo");
   if (await codify({
     origin: {
-      smt: "xlsx|./test/data/input/foofile.xlsx|foo|*"
+      smt: "xlsx|./test/data/input/foofile.xlsx|foo|*",
+      options: {}
     },
     output: './test/data/output/xlsx/codify_xlsx.json'
   })) return 1;
@@ -22,9 +23,26 @@ async function tests() {
   logger.info("=== codify foofile.xls|foo");
   if (await codify({
     origin: {
-      smt: "xls|./test/data/input/foofile.xls|foo|*"
+      smt: "xls|./test/data/input/foofile.xls|foo|*",
+      options: {
+        cellDates: true
+      }
     },
     output: './test/data/output/xlsx/codify_xls.json'
+  })) return 1;
+
+  logger.info("=== codify voter registration");
+  if (await codify({
+    origin: {
+      smt: "xlsx|./test/data/input/State_Voter_Registration_2024_PPE.xlsx|in|*",
+      options: {
+        cellDates: false,
+        heading: "Active",
+        cells: 9,
+        column: 0
+      }
+    },
+    output: './test/data/output/xlsx/codify_svr.json'
   })) return 1;
 
 }
